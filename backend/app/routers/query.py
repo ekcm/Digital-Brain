56341024d9eadd21549747d52aca3f821a3f8111
Elijah Khor <elijahkcm@gmail.com>
2025-02-07 23:52:47 -0800

    add dockerfiles, add docker-compose

--- backend/app/routers/query.py
+++ backend/app/routers/query.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class QueryRequest(BaseModel):
     message: str
@@ -9,3 +13,8 @@ router = APIRouter()
 @router.post("/query", tags=["query"])
 async def query(request: QueryRequest):
     return {"message": request.message + " (from backend)"}
+
+@router.get("/openai", tags=["query"])
+async def openai():
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    return {"message": f"Hello from OpenAI: {OPENAI_API_KEY}"}
